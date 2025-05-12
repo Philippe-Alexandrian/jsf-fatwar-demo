cd33fb3e6726349100c1c0e50c8b8b82efcddb91
Philippe Alexandrian <philippe.alexandrian@ac-toulouse.fr>
2025-05-12 17:03:33 +0200

    Remove Cdi beans and associated conf.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,10 @@
 package com.example.demo;
 
-import jakarta.inject.Named;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-// ExcludeFilters needed in order for Spring not to scan Cdi Bean.
-@ComponentScan(basePackages = "com.example.demo", excludeFilters = { @Filter(type = FilterType.ANNOTATION, classes = Named.class) })
 public class DemoApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
